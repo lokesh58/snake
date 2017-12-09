81dec82437c958efd30ac97183edc6681f4c2b34
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-09 16:19:13 +0530

    main.cc work completed only work in the headers folder is needed

--- main.cc
+++ main.cc
@@ -1,10 +1,26 @@
 #include <iostream>
-
 using namespace std;
+#include <string.h>
+#include "headers//Game.h"
 
 int main() {
-	cout << "######################Welcome to snake game############################" << endl;
+	Game snake;
+	bool play = true;
 	
+	while (play) {
+		snake.play();
+		cout << "Do you want to play another round? (y/n): ";
+		char choice;
+		cin >> choice;
+		choice = tolower(choice);
+		while (choice != 'y' && choice != 'n') {
+			cout << "Your choice is not valid! Enter again: ";
+			cin >> choice;
+			choice = tolower(choice);
+		}
+		play = (choice == 'y');
+	}
+	cout << "\n\nThanks for playing my game! Hope you enjoyed :)" << endl;
 	
 
 	return 0;
