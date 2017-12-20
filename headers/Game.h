0cd3872abf09a73744a4b13bf2898bb571a3134b
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-20 13:09:48 +0530

    Added function to get random integer between height and width

--- headers/Game.h
+++ headers/Game.h
@@ -2,6 +2,7 @@
 #define GAME_H
 
 #include <utility>
+#include <random>
 #include "Snake.h"
 
 class Game {
@@ -14,8 +15,13 @@ class Game {
 		void reset();
 		void gameLoop();
 
+		int getRandomInt();
+
 		std::unique_ptr<Snake> head;
 		int foodX, foodY;
+
+		std::uniform_int_distriubution<int> _uid;
+		std::default_random_engine _e;
 };
 
 #endif //GAME_H
