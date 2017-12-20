810a7c060b4bf51aeca9437d69da40152e595c63
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-20 11:31:57 +0530

    Added the defintions for move() and setPos()

--- headers/Snake.cc
+++ headers/Snake.cc
@@ -1,9 +1,32 @@
 #include "Snake.h"
 
-Snake::Snake() {
+Snake::Snake() : _x(0), _y(0), _prev(nullptr) {
+	//ctor
+}
 
+Snake::Snake(int x, int y) : _x(x), _y(y), _prev(nullptr) {
+	//ctor
 }
 
 Snake::~Snake() {
+	//dtor
+}
 
+void Snake::addTail() {
+	//TODO
+}
+
+void Snake::move(Direction delta) {
+	if (_prev != nullptr) {
+		_prev->setPos(_x, _y);
+	}
+	_x += delta.first;
+	_y += delta.second;
+}
+
+void Snake::setPos(int x, int y) {
+	if (_prev != nullptr) {
+		_prev->setPos(_x, _y);
+	}
+	_x = x, _y = y;
 }
