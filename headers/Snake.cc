afc4bfd9bdf1064ee9e08fc379f3dcf5203d8ab6
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-20 11:55:25 +0530

    Made slight changes to addTail()

--- headers/Snake.cc
+++ headers/Snake.cc
@@ -15,10 +15,10 @@ Snake::~Snake() {
 void Snake::addTail() {
 	if (_prev == nullptr) {
 		_prev = std::make_unique<Snake>(_x, _y+1);
-	} else if (_prev->_prev != nullptr) {
-		_prev->addTail();
-	} else {
+	} else if (_prev->_prev == nullptr) {
 		_prev->_prev = std::make_unique<Snake>(2*(_prev->getX()) - getX(), 2*(_prev->getY()) - getY());
+	} else {
+		_prev->addTail();
 	}
 }
 
