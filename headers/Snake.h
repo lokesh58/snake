f6ff66897bfb11d8b9ddd5aafe14d73e1a788d45
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-20 11:31:25 +0530

    Added move(), setPos() and addTail() function declarations in class Snake

--- headers/Snake.h
+++ headers/Snake.h
@@ -1,12 +1,24 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+#include <memory>
+#include <utility>
+
 class Snake {
+	using Direction = std::pair<int, int>;
+
 	public:
 		Snake();
+		Snake(int x, int y);
 		~Snake();
+		void addTail();
+		void move(Direction delta);
 
 	private:
+		void setPos(int x, int y);
+
+		int _x, _y;
+		std::unique_ptr<Snake> _prev;
 };
 
 #endif //SNAKE_H
