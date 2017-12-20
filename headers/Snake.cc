0fc5b14fd6eca2c70adf1b8bcee4e9f04aa1f539
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-20 11:51:42 +0530

    Added the definitions of getX() and getY()

--- headers/Snake.cc
+++ headers/Snake.cc
@@ -13,15 +13,21 @@ Snake::~Snake() {
 }
 
 void Snake::addTail() {
-	//TODO
+	if (_prev == nullptr) {
+		_prev = std::make_unique<Snake>(_x, _y+1);
+	} else if (_prev->_prev != nullptr) {
+		_prev->addTail();
+	} else {
+		_prev->_prev = std::make_unique<Snake>(2*(_prev->getX()) - getX(), 2*(_prev->getY()) - getY());
+	}
 }
 
-void Snake::move(Direction delta) {
+void Snake::move(int dx, int dy) {
 	if (_prev != nullptr) {
 		_prev->setPos(_x, _y);
 	}
-	_x += delta.first;
-	_y += delta.second;
+	_x += dx;
+	_y += dy;
 }
 
 void Snake::setPos(int x, int y) {
@@ -29,4 +35,12 @@ void Snake::setPos(int x, int y) {
 		_prev->setPos(_x, _y);
 	}
 	_x = x, _y = y;
+}
+
+int Snake::getX() const {
+	return _x;
+}
+
+int Snake::getY() const {
+	return _y;
 }
