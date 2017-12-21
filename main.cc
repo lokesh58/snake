65b1892bc6df7be69e9ac992d9f5d95f899553dc
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-21 13:54:41 +0530

    Moving the part in main to Game::play()

--- main.cc
+++ main.cc
@@ -1,27 +1,10 @@
 #include <iostream>
 using namespace std;
 #include <string.h>
-#include "headers//Game.h"
+#include "headers/Game.h"
 
 int main() {
 	Game snake;
-	bool play = true;
-	
-	while (play) {
-		snake.play();
-		cout << "Do you want to play another round? (y/n): ";
-		char choice;
-		cin >> choice;
-		choice = tolower(choice);
-		while (choice != 'y' && choice != 'n') {
-			cout << "Your choice is not valid! Enter again: ";
-			cin >> choice;
-			choice = tolower(choice);
-		}
-		play = (choice == 'y');
-	}
-	cout << "\n\nThanks for playing my game! Hope you enjoyed :)" << endl;
-	
 
 	return 0;
 }
